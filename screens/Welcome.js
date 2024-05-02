ccecfb8326633eef773c1f4a77ca02af0346966e
Jtangyi <168657689+Jtangyi@users.noreply.github.com>
2024-05-02 03:31:43 -0700

    Update Welcome.js

--- screens/Welcome.js
+++ screens/Welcome.js
@@ -1,148 +1,43 @@
-import { View, Text, Pressable, Image } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { View, Text, Pressable, Image, StyleSheet } from 'react-native';
 import { LinearGradient } from "expo-linear-gradient";
 import COLORS from '../constants/colors';
 import Button from '../components/Button';
 
 const Welcome = ({ navigation }) => {
-
     return (
         <LinearGradient
-            style={{
-                flex: 1
-            }}
-            colors={[COLORS.secondary, COLORS.primary]}
+            style={{ flex: 1 }}
+            colors={['#FFFFFF', '#FFFFFF']}
         >
             <View style={{ flex: 1 }}>
-                <View>
+                {/* Logo */}
+                <View style={styles.logoContainer}>
                     <Image
-                        source={require("../assets/hero1.jpg")}
-                        style={{
-                            height: 100,
-                            width: 100,
-                            borderRadius: 20,
-                            position: "absolute",
-                            top: 10,
-                            transform: [
-                                { translateX: 20 },
-                                { translateY: 50 },
-                                { rotate: "-15deg" }
-                            ]
-                        }}
-                    />
-
-                    <Image
-                        source={require("../assets/hero3.jpg")}
-                        style={{
-                            height: 100,
-                            width: 100,
-                            borderRadius: 20,
-                            position: "absolute",
-                            top: -30,
-                            left: 100,
-                            transform: [
-                                { translateX: 50 },
-                                { translateY: 50 },
-                                { rotate: "-5deg" }
-                            ]
-                        }}
-                    />
-
-                    <Image
-                        source={require("../assets/hero3.jpg")}
-                        style={{
-                            width: 100,
-                            height: 100,
-                            borderRadius: 20,
-                            position: "absolute",
-                            top: 130,
-                            left: -50,
-                            transform: [
-                                { translateX: 50 },
-                                { translateY: 50 },
-                                { rotate: "15deg" }
-                            ]
-                        }}
-                    />
-
-                    <Image
-                        source={require("../assets/hero2.jpg")}
-                        style={{
-                            height: 200,
-                            width: 200,
-                            borderRadius: 20,
-                            position: "absolute",
-                            top: 110,
-                            left: 100,
-                            transform: [
-                                { translateX: 50 },
-                                { translateY: 50 },
-                                { rotate: "-15deg" }
-                            ]
-                        }}
+                        source={require("../assets/facebook.png")}
+                        style={styles.logo}
+                        resizeMode="contain"
                     />
                 </View>
 
-                {/* content  */}
-
-                <View style={{
-                    paddingHorizontal: 22,
-                    position: "absolute",
-                    top: 400,
-                    width: "100%"
-                }}>
-                    <Text style={{
-                        fontSize: 50,
-                        fontWeight: 800,
-                        color: COLORS.white
-                    }}>Let's Get</Text>
-                    <Text style={{
-                        fontSize: 46,
-                        fontWeight: 800,
-                        color: COLORS.white
-                    }}>Started</Text>
-
-                    <View style={{ marginVertical: 22 }}>
-                        <Text style={{
-                            fontSize: 16,
-                            color: COLORS.white,
-                            marginVertical: 4
-                        }}>Connect to Nature with woosh</Text>
-                        <Text style={{
-                            fontSize: 16,
-                            color: COLORS.white,
-                        }}>Shop whoosh products, Straight from nature</Text>
+                {/* Content */}
+                <View style={styles.contentContainer}>
+                    <Text style={styles.heading}>Let's Get</Text>
+                    <Text style={styles.heading}>Started</Text>
+                    <View style={styles.sloganContainer}>
+                        <Text style={styles.slogan}>Connect to Nature with woosh</Text>
+                        <Text style={styles.slogan}>Shop woosh products, Straight from nature</Text>
                     </View>
-
                     <Button
                         title="Join Now"
                         onPress={() => navigation.navigate("Signup")}
-                        style={{
-                            marginTop: 22,
-                            width: "100%"
-                        }}
+                        style={styles.button}
                     />
-
-                    <View style={{
-                        flexDirection: "row",
-                        marginTop: 12,
-                        justifyContent: "center"
-                    }}>
-                        <Text style={{
-                            fontSize: 16,
-                            color: COLORS.white
-                        }}>Already have an account ?</Text>
-                        <Pressable
-                            onPress={() => navigation.navigate("Login")}
-                        >
-                            <Text style={{
-                                fontSize: 16,
-                                color: COLORS.white,
-                                fontWeight: "bold",
-                                marginLeft: 4
-                            }}>Login</Text>
+                    <View style={styles.loginContainer}>
+                        <Text style={styles.loginText}>Already have an account?</Text>
+                        <Pressable onPress={() => navigation.navigate("Login")}>
+                            <Text style={[styles.loginText, { color: COLORS.grey, fontWeight: "bold", marginLeft: 4 }]}>Login</Text>
                         </Pressable>
-
                     </View>
                 </View>
             </View>
@@ -150,4 +45,47 @@ const Welcome = ({ navigation }) => {
     )
 }
 
-export default Welcome
+const styles = StyleSheet.create({
+    logoContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    logo: {
+        width: 150, // Adjust the width of the logo as needed
+        height: 150, // Adjust the height of the logo as needed
+    },
+    contentContainer: {
+        paddingHorizontal: 22,
+        paddingBottom: 50,
+    },
+    heading: {
+        fontSize: 50,
+        fontWeight: '800',
+        color: COLORS.black,
+    },
+    sloganContainer: {
+        marginVertical: 22,
+    },
+    slogan: {
+        fontSize: 16,
+        color: COLORS.black,
+        marginVertical: 4,
+    },
+    button: {
+        marginTop: 22,
+        width: "100%",
+        backgroundColor: COLORS.black,
+    },
+    loginContainer: {
+        flexDirection: "row",
+        marginTop: 12,
+        justifyContent: "center",
+    },
+    loginText: {
+        fontSize: 16,
+        color: COLORS.black,
+    },
+});
+
+export default Welcome;
